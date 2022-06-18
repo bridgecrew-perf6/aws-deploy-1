d845b48255696da8130451a8f07d46edfef2c19a
johnd <justjohnd@gmail.com>
2022-06-18 21:03:51 +0900

    Set up frontend to communicate with backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    getNames();
+  }, []);
+
+  const getNames = async () => {
+    const response = await axios.get("/names");
+    setUserName(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <h2>My name is {userName}</h2>
     </div>
   );
 }
